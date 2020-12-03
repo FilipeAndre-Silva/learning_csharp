bb2f0278c77d7dfb93c3f0415c8e1a883cbfd790
FilipeAndre-Silva <filipeandre.private@gmail.com>
2020-12-02 22:04:32 -0300

    Primeiro commit aprendendo C#

---
+++ Introdução CSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Introdução_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Olá Mundo do C# !");
+            Console.WriteLine("WriteLine tem quebra de linha !");
+
+            Console.Write("Write Não tem quebra de linhas...");
+            Console.Write("Simples assim..");
+
+            //Forma de comentar em C#
+            
+            /* 
+            Outra forma de comentar em C#,
+            Em várias linhas...
+            */
+        }
+    }
+}
